7d6a3a92bfd68793475980b258ce5bccba483f57
Eli Oshinsky <elimelech.oshinsky@gmail.com>
2017-01-18 21:35:51 -0500

    updated to use Jquery for DOM handling

--- interface.js
+++ interface.js
@@ -1,34 +1,25 @@
-function $(identifier) {
-    if (identifier.charAt(0) == "#") {
-        return document.getElementById(identifier.slice(1));
-    } else if (identifier.charAt(0) == ".") {
-        return document.getElementsByClassName(identifier.slice(1));
-    } else {
-        return document.getElementsbyTagName(identifier);
-    }
-}
-
 function addNewDrink() {
-  addNewDrunk($('#new-drink-name').value, $('#new-drink-percent').value, $('#new-drink-volume').value);
-  $('#new-drink-name').value = '';
-  $('#new-drink-percent').value = '';
-  $('#new-drink-volume').value = '';
+  addNewDrunk($('#new-drink-name').val(), $('#new-drink-percent').val(), $('#new-drink-volume').val());
+  $('#new-drink-name').val('');
+  $('#new-drink-percent').val('');
+  $('#new-drink-volume').val('');
 }
-$('#submit').addEventListener('click', addNewDrink);
-$('#custom-drink').addEventListener('keydown', function(){if (window.event.keyCode == 13) addNewDrink();});
+$('#submit').click(addNewDrink);
+$('#custom-drink').on('keydown', function(){if (event.keyCode == 13) addNewDrink()});
 
 /*  for (var i = 0; i < drinks.length; i++) {
     $('#drink-list').innerHTML += '<li id="' + drinks[i].drink + '">' + drinks[i].drink + '</li>';
     $('#' + drinks[i].drink).addEventListener('click', addNewDrunk(drinks[i].drink));
   }*/
 
+
 function addNewDrunk(name, percentage, vol){
   var drinkNames = "";
   addDrunk(name, percentage, vol);
   for(var i = 0; i < drunk.length; i++){
     if (drinkNames.length < 2) drinkNames += drunk[i].drink;
-    else drinkNames += ', ' + drunk[i].drink;
-    drinkNames += ' : ' + drunk[i].number;
+    else drinkNames += '<div>' + drunk[i].drink;
+    drinkNames += ' : ' + drunk[i].number + '</div>';
+    $('#display-data').html(drinkNames);
   }
-  $('#display-data').innerHTML= drinkNames;
 }
